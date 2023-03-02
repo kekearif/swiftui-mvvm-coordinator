f5b28a1dbeadd8e2641b397cdc65223b1b0c039a
Keke <kekearif@googlemail.com>
2023-03-01 18:13:42 -0600

    Initial Commit

---
+++ swiftui-mvvm-coordinator/swiftui_mvvm_coordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvm_coordinatorApp.swift
+//  swiftui-mvvm-coordinator
+//
+//  Created by Keke Arif on 2023/3/1.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvm_coordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
